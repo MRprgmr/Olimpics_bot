685dfc8efb1749a9b31ad97392eb55785448e763
MRprgmr <algoprogram01@gmail.com>
2021-09-04 13:54:10 +0500

    Uzbek locale was set

--- Olimpics_bot/wsgi.py
+++ Olimpics_bot/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Olimpics_bot project.
-
+WSGI config for Taxi_bot project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+import locale
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Olimpics_bot.settings')
+locale.setlocale(locale.LC_ALL, 'uz_UZ.UTF-8')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taxi_bot.settings')
 
 application = get_wsgi_application()
